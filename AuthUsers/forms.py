2a1cfac2aa8950d3c84f3ac330af3225274e12ca
Manuel Bayona <manudesarrolla07@gmail.com>
2022-08-15 21:27:54 -0400

    Delete AuthUsers directory

--- AuthUsers/forms.py
+++
@@ -1,8 +0,0 @@
-#Form Design:
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
