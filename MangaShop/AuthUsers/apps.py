e56143754f297dfce9a4790f395f06860350db3a
Manuel Bayona <manudesarrolla07@gmail.com>
2022-08-23 12:50:34 -0400

    Delete MangaShop directory

--- MangaShop/AuthUsers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthusersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AuthUsers'
--- MangaShop/AuthUsers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from AuthUsers import views
-
-urlpatterns = [
-    path('register/', views.register_user, name='registrar'),
-    path('login/', views.login_user, name='ingresar'),
-    path('logout/', views.logout_user, name='cerrar-sesion')
-]
