ea9f8b6e8c730cc7dc0acf2626d302ca5d4a1724
Manuel Bayona <manudesarrolla07@gmail.com>
2022-08-23 12:51:15 -0400

    Delete PasswordGenerator (completo) directory

--- PasswordGenerator (completo)/mainApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from mainApp import views
-
-urlpatterns = [
-    path('', views.index, name='inicio'),
-    path('show-password/', views.show_password, name='generador_contraseña')
-]
